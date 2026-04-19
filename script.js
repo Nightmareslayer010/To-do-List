3218849174bda2b549295b4641be15940fe058d7
Giriraj <girirajvyas010@gmail.com>
2026-04-19 17:48:14 +0530

    declared variables

---
+++ script.js
@@ -0,0 +1,11 @@
+//variables
+let tasks = [];
+let searchfield = document.querySelector(".Search");
+let actionbtn = document.querySelector(".action-btn");
+let allbtn = document.querySelector(".allbtn");
+let activebtn = document.querySelector(".activebtn");
+let completedbtn = document.querySelector(".completedbtn");
+let inputfield = document.querySelector(".input-task");
+let date = document.querySelector(".dueDate");
+let addbtn = document.querySelector(".add-btn");
+let tasklist = document.querySelector(".tasklist");
